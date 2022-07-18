fa808a6a48a5eb042cb9febac3be60f9d2f85dea
blunderbeard <jsmith@greyavocado.io>
2022-07-18 01:36:16 -0400

    challenge 13

--- myApp.js
+++ myApp.js
@@ -1,4 +1,5 @@
 let express = require('express');
+let bodyParser = require('body-parser');
 let app = express();
 let index = __dirname + '/views/index.html';
 
@@ -6,6 +7,8 @@ let index = __dirname + '/views/index.html';
 app.use(express.static(__dirname + "/public"));
 app.use("/public", express.static(__dirname + "/public"));
 
+//Body-parser
+app.use(bodyParser.urlencoded({extended: false}));
 
 //Remote Logger
 app.use((req, res, next) => {
